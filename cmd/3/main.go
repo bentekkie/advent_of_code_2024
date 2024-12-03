c11dc447d875cb4382d375ef7fbc9d88c7b5e550
Benjamin Segall <bentekkie@gmail.com>
2024-12-03 07:23:39 -0500

    Day 3

--- cmd/3/main.go
+++ cmd/3/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/bentekkie/advent_of_code_2024/pkg/inputs"
 )
@@ -15,11 +18,34 @@ func main() {
 }
 
 func part1(input string) {
-    // TODO: implement me
-	fmt.Printf("Part 1: \n")
+	r, _ := regexp.Compile(`mul\((\d+),(\d+)\)`)
+	res := r.FindAllStringSubmatch(input, -1)
+	sum := 0
+	for _, match := range res {
+		a, _ := strconv.Atoi(match[1])
+		b, _ := strconv.Atoi(match[2])
+		sum += a * b
+	}
+	fmt.Printf("Part 1: %v\n", sum)
 }
 
 func part2(input string) {
-    // TODO: implement me
-	fmt.Printf("Part 2: \n")
+	r, _ := regexp.Compile(`mul\((\d+),(\d+)\)|do\(\)|don't\(\)`)
+	res := r.FindAllStringSubmatch(input, -1)
+	sum := 0
+	enabled := true
+	for _, match := range res {
+		if match[0] == "do()" {
+			enabled = true
+		}
+		if match[0] == "don't()" {
+			enabled = false
+		}
+		if strings.HasPrefix(match[0], "mul") && enabled {
+			a, _ := strconv.Atoi(match[1])
+			b, _ := strconv.Atoi(match[2])
+			sum += a * b
+		}
+	}
+	fmt.Printf("Part 2: %v\n", sum)
 }
