63e8e01e88e76d8585b6a6adf30c9e41d94f9fa3
Benjamin Segall <bentekkie@gmail.com>
2024-12-04 09:13:21 -0500

    Clean up day 4

--- cmd/4/main.go
+++ cmd/4/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"iter"
+	"regexp"
 	"strings"
 
-	"github.com/bentekkie/advent_of_code_2024/pkg/benlog"
 	"github.com/bentekkie/advent_of_code_2024/pkg/inputs"
 )
 
@@ -16,98 +17,77 @@ func main() {
 	part2(input)
 }
 
+var xmas = regexp.MustCompile("XMAS")
+var samx = regexp.MustCompile("SAMX")
+
 func checkLine(line string) int {
-	c := 0
-	for i := range len(line) - 3 {
-		if line[i:i+4] == "XMAS" {
-			c++
-		} else if line[i:i+4] == "SAMX" {
-			c++
-		}
-	}
-	return c
+	return len(xmas.FindAllString(line, -1)) + len(samx.FindAllString(line, -1))
 }
 
 func part1(input string) {
 	lines := strings.Split(strings.TrimSpace(input), "\n")
+	rlines := make([]strings.Builder, len(lines[0]))
 	sum := 0
 	for _, line := range lines {
-		sum += checkLine(line)
-	}
-	rlines := make([]string, 0, len(lines[0]))
-	for range len(lines[0]) {
-		rlines = append(rlines, "")
-	}
-	for _, line := range lines {
 		for i, c := range line {
-			if strings.TrimSpace(string(c)) == "" {
-				continue
-			}
-			rlines[i] += string(c)
+			rlines[i].WriteRune(c)
 		}
+		sum += checkLine(line)
 	}
 	for _, line := range rlines {
-		sum += checkLine(line)
+		sum += checkLine(line.String())
 	}
-	diags := []string{}
-	for k := range len(lines) + len(lines[0]) {
+	var d, dr strings.Builder
+	for k := range len(lines) + len(lines[0]) - 1 {
+		d.Reset()
+		dr.Reset()
 		y := k
 		x := 0
-		diag := ""
+		rx := len(lines[0]) - 1
 		if y >= len(lines) {
-			x = y - len(lines)
+			x = y - len(lines) + 1
+			rx -= x
 			y = len(lines) - 1
 		}
-		for ; x < len(lines[0]) && y >= 0; x, y = x+1, y-1 {
-			diag += string(lines[y][x])
+		for ; x < len(lines[0]) && y >= 0; x, rx, y = x+1, rx-1, y-1 {
+			d.WriteByte(lines[y][x])
+			dr.WriteByte(lines[y][rx])
 		}
-		if k == 0 || diag != diags[len(diags)-1] {
-			diags = append(diags, diag)
-		}
-	}
-	for _, line := range diags {
-		sum += checkLine(line)
-	}
-	rdiags := []string{}
-	for k := range len(lines) + len(lines[0]) {
-		y := k
-		x := len(lines[0]) - 1
-		diag := ""
-		if y >= len(lines) {
-			x = len(lines[0]) - 1 - (y - len(lines))
-			y = len(lines) - 1
-		}
-		for ; x >= 0 && y >= 0; x, y = x-1, y-1 {
-			diag += string(lines[y][x])
-		}
-		if k == 0 || diag != rdiags[len(rdiags)-1] {
-			rdiags = append(rdiags, diag)
-		}
-	}
-	for _, line := range rdiags {
-		sum += checkLine(line)
+		sum += checkLine(d.String()) + checkLine(dr.String())
 	}
 	fmt.Printf("Part 1: %d\n", sum)
 }
 
 func check3x3MAS(lines []string) bool {
-	return lines[1][1] == 'A' && ((lines[0][0] == 'M' && lines[2][2] == 'S') || (lines[0][0] == 'S' && lines[2][2] == 'M')) && ((lines[0][2] == 'M' && lines[2][0] == 'S') || (lines[0][2] == 'S' && lines[2][0] == 'M'))
+	return lines[1][1] == 'A' &&
+		((lines[0][0] == 'M' && lines[2][2] == 'S') ||
+			(lines[0][0] == 'S' && lines[2][2] == 'M')) &&
+		((lines[0][2] == 'M' && lines[2][0] == 'S') ||
+			(lines[0][2] == 'S' && lines[2][0] == 'M'))
+}
+
+func boxes(lines []string) iter.Seq[[]string] {
+	return func(yield func([]string) bool) {
+		for y := range len(lines) - 2 {
+			for x := range len(lines[y]) - 2 {
+				if !yield([]string{
+					lines[y][x : x+3],
+					lines[y+1][x : x+3],
+					lines[y+2][x : x+3],
+				}) {
+					return
+				}
+			}
+		}
+	}
 }
 
 func part2(input string) {
 	lines := strings.Split(strings.TrimSpace(input), "\n")
 	c := 0
-	for y := range len(lines) - 2 {
-		for x := range len(lines[y]) - 2 {
-			box := []string{
-				lines[y][x : x+3],
-				lines[y+1][x : x+3],
-				lines[y+2][x : x+3],
-			}
-			benlog.ExamplePrintf("%s\n\n", strings.Join(box, "\n"))
-			if check3x3MAS(box) {
-				c++
-			}
+	for box := range boxes(lines) {
+		if check3x3MAS(box) {
+			c++
 		}
 	}
 	fmt.Printf("Part 2: %d\n", c)
