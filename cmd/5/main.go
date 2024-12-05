5f74a6e0c0322973811e13e48692b34905cd4885
Benjamin Segall <bentekkie@gmail.com>
2024-12-05 07:41:37 -0500

    Day 5

--- cmd/5/main.go
+++ cmd/5/main.go
@@ -3,23 +3,88 @@ package main
 import (
 	"flag"
 	"fmt"
+	"iter"
+	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/bentekkie/advent_of_code_2024/pkg/inputs"
 )
 
 func main() {
 	flag.Parse()
-	input := inputs.String()
-	part1(input)
-	part2(input)
+	part1(inputs.Lines())
+	part2(inputs.Lines())
 }
 
-func part1(input string) {
-    // TODO: implement me
-	fmt.Printf("Part 1: \n")
+func parseInput(lines iter.Seq[string]) (map[int]map[int]struct{}, [][]int) {
+	updates := [][]int{}
+	addingRules := true
+	after := map[int]map[int]struct{}{}
+	for line := range lines {
+		line := strings.TrimSpace(line)
+		if line == "" {
+			addingRules = false
+			continue
+		}
+		if addingRules {
+			parts := strings.Split(line, "|")
+			left, _ := strconv.Atoi(strings.TrimSpace(parts[0]))
+			right, _ := strconv.Atoi(strings.TrimSpace(parts[1]))
+			if _, ok := after[left]; !ok {
+				after[left] = map[int]struct{}{}
+			}
+			after[left][right] = struct{}{}
+		} else {
+			parts := strings.Split(line, ",")
+			update := []int{}
+			for _, part := range parts {
+				p, _ := strconv.Atoi(strings.TrimSpace(part))
+				update = append(update, p)
+			}
+			updates = append(updates, update)
+		}
+	}
+	return after, updates
 }
 
-func part2(input string) {
-    // TODO: implement me
-	fmt.Printf("Part 2: \n")
+func part1(lines iter.Seq[string]) {
+	after, updates := parseInput(lines)
+	sum := 0
+	sortFunc := func(a, b int) int {
+		if _, ok := after[a][b]; ok {
+			return -1
+		} else if _, ok := after[b][a]; ok {
+			return 1
+		} else {
+			return 0
+		}
+	}
+	for _, update := range updates {
+		if slices.IsSortedFunc(update, sortFunc) {
+			sum += update[len(update)/2]
+		}
+	}
+	fmt.Printf("Part 1: %d\n", sum)
+}
+
+func part2(lines iter.Seq[string]) {
+	after, updates := parseInput(lines)
+	sum := 0
+	sortFunc := func(a, b int) int {
+		if _, ok := after[a][b]; ok {
+			return -1
+		} else if _, ok := after[b][a]; ok {
+			return 1
+		} else {
+			return 0
+		}
+	}
+	for _, update := range updates {
+		if !slices.IsSortedFunc(update, sortFunc) {
+			slices.SortFunc(update, sortFunc)
+			sum += update[len(update)/2]
+		}
+	}
+	fmt.Printf("Part 2: %d\n", sum)
 }
