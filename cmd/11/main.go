f5a848978ca93f073b44e5c1d492caa430ff333d
Benjamin Segall <bentekkie@gmail.com>
2024-12-11 07:15:12 -0500

    Day 11

--- cmd/11/main.go
+++ cmd/11/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/bentekkie/advent_of_code_2024/pkg/inputs"
 )
@@ -15,11 +17,61 @@ func main() {
 }
 
 func part1(input string) {
-    // TODO: implement me
-	fmt.Printf("Part 1: \n")
+	stones := strings.Split(strings.TrimSpace(input), " ")
+	//fmt.Printf("%v\n", stones)
+	for range 25 {
+		newStones := []string{}
+		for _, stone := range stones {
+			if stone == "0" {
+				newStones = append(newStones, "1")
+			} else if len(stone)%2 == 0 {
+				left, right := stone[:len(stone)/2], stone[len(stone)/2:]
+				for right[0] == '0' && len(right) > 1 {
+					right = right[1:]
+				}
+				newStones = append(newStones, left, right)
+			} else {
+				num, _ := strconv.Atoi(stone)
+				newStones = append(newStones, strconv.Itoa(num*2024))
+			}
+		}
+		stones = newStones
+		//fmt.Printf("%v\n", stones)
+	}
+	// TODO: implement me
+	fmt.Printf("Part 1: %d\n", len(stones))
 }
 
 func part2(input string) {
-    // TODO: implement me
-	fmt.Printf("Part 2: \n")
+	parts := strings.Split(strings.TrimSpace(input), " ")
+	stones := map[string]int{}
+	for _, p := range parts {
+		stones[p]++
+	}
+	for range 75 {
+		newStones := map[string]int{}
+		for stone, count := range stones {
+			if stone == "0" {
+				newStones["1"] += count
+			} else if len(stone)%2 == 0 {
+				left, right := stone[:len(stone)/2], stone[len(stone)/2:]
+				for right[0] == '0' && len(right) > 1 {
+					right = right[1:]
+				}
+				newStones[left] += count
+				newStones[right] += count
+			} else {
+				num, _ := strconv.Atoi(stone)
+				newStones[strconv.Itoa(num*2024)] += count
+			}
+		}
+		stones = newStones
+		//fmt.Printf("%v\n", stones)
+	}
+	total := 0
+	for _, c := range stones {
+		total += c
+	}
+	// TODO: implement me
+	fmt.Printf("Part 2: %d\n", total)
 }
