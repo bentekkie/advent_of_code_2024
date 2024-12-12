6f031e2466b4ef9563980e4455419a45a7b6ef34
Benjamin Segall <bentekkie@gmail.com>
2024-12-12 07:42:03 -0500

    Day 12

--- cmd/11/main.go
+++ cmd/11/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 
+	"github.com/bentekkie/advent_of_code_2024/pkg/benlog"
 	"github.com/bentekkie/advent_of_code_2024/pkg/inputs"
 )
 
@@ -13,7 +15,12 @@ func main() {
 	flag.Parse()
 	input := inputs.String()
 	part1(input)
-	part2(input)
+	benlog.Timed(func() {
+		part2(input)
+	})
+	benlog.Timed(func() {
+		part2strmul(input)
+	})
 }
 
 func part1(input string) {
@@ -74,4 +81,77 @@ func part2(input string) {
 	}
 	// TODO: implement me
 	fmt.Printf("Part 2: %d\n", total)
+}
+
+func part2strmul(input string) {
+	parts := strings.Split(strings.TrimSpace(input), " ")
+	stones := map[int]int{}
+	for _, p := range parts {
+		n, _ := strconv.Atoi(p)
+		stones[n]++
+	}
+	for range 75 {
+		newStones := map[int]int{}
+		for stone, count := range stones {
+			if stone == 0 {
+				newStones[1] += count
+			} else if l, r, ok := split(stone); ok {
+				newStones[l] += count
+				newStones[r] += count
+			} else {
+				newStones[2024*stone] += count
+			}
+		}
+		stones = newStones
+		//fmt.Printf("%v\n", stones)
+	}
+	total := 0
+	for _, c := range stones {
+		total += c
+	}
+	// TODO: implement me
+	fmt.Printf("Part 2: %d\n", total)
+}
+
+func split(stone int) (int, int, bool) {
+	dl := diglen(stone)
+	if dl%2 == 1 {
+		return 0, 0, false
+	}
+	ten := int(math.Pow10(dl / 2))
+	return stone / ten, stone % ten, true
+}
+
+func diglen(a int) int {
+	return int(math.Floor(math.Log10(float64(a)))) + 1
+}
+
+func strMul(a int, b string) int {
+	total := 0
+	place := 1
+	for i := len(b) - 1; i >= 0; i-- {
+		switch b[i] {
+		case '0':
+		case '1':
+			total += place * a
+		case '2':
+			total += place * 2 * a
+		case '3':
+			total += place * 3 * a
+		case '4':
+			total += place * 4 * a
+		case '5':
+			total += place * 5 * a
+		case '6':
+			total += place * 6 * a
+		case '7':
+			total += place * 7 * a
+		case '8':
+			total += place * 8 * a
+		case '9':
+			total += place * 9 * a
+		}
+		place *= 10
+	}
+	return total
 }
--- cmd/12/main.go
+++ cmd/12/main.go
@@ -5,21 +5,174 @@ import (
 	"fmt"
 
 	"github.com/bentekkie/advent_of_code_2024/pkg/inputs"
+	"gonum.org/v1/gonum/graph"
+	"gonum.org/v1/gonum/graph/simple"
+	"gonum.org/v1/gonum/graph/topo"
 )
 
 func main() {
 	flag.Parse()
-	input := inputs.String()
-	part1(input)
-	part2(input)
+	part1(&inputs.Grid{})
+	part2(&inputs.Grid{})
 }
 
-func part1(input string) {
-    // TODO: implement me
-	fmt.Printf("Part 1: \n")
+func part1(input *inputs.Grid) {
+	locs := map[complex128]rune{}
+	g := simple.NewUndirectedGraph()
+	nodes := map[complex128]graph.Node{}
+	locsFromNodeIds := map[int64]complex128{}
+	for loc, c := range input.Locs() {
+		locs[loc] = c
+		nodes[loc] = g.NewNode()
+		g.AddNode(nodes[loc])
+		locsFromNodeIds[nodes[loc].ID()] = loc
+	}
+	for loc, c := range locs {
+		if other, ok := locs[loc-1]; ok && c == other {
+			g.SetEdge(g.NewEdge(nodes[loc], nodes[loc-1]))
+		}
+		if other, ok := locs[loc+1]; ok && c == other {
+			g.SetEdge(g.NewEdge(nodes[loc], nodes[loc+1]))
+		}
+		if other, ok := locs[loc-1i]; ok && c == other {
+			g.SetEdge(g.NewEdge(nodes[loc], nodes[loc-1i]))
+		}
+		if other, ok := locs[loc+1i]; ok && c == other {
+			g.SetEdge(g.NewEdge(nodes[loc], nodes[loc+1i]))
+		}
+	}
+	s := 0
+	for _, component := range topo.ConnectedComponents(g) {
+		area := len(component)
+		compLocs := make(map[complex128]struct{}, len(component))
+		for _, n := range component {
+			compLocs[locsFromNodeIds[n.ID()]] = struct{}{}
+		}
+		perim := 0
+		for loc := range compLocs {
+			if _, ok := compLocs[loc-1]; !ok {
+				perim++
+			}
+			if _, ok := compLocs[loc+1]; !ok {
+				perim++
+			}
+			if _, ok := compLocs[loc-1i]; !ok {
+				perim++
+			}
+			if _, ok := compLocs[loc+1i]; !ok {
+				perim++
+			}
+		}
+		s += area * perim
+	}
+	// TODO: implement me
+	fmt.Printf("Part 1: %d\n", s)
 }
 
-func part2(input string) {
-    // TODO: implement me
-	fmt.Printf("Part 2: \n")
+type Dir int
+
+const (
+	V Dir = iota
+	H
+)
+
+type side struct {
+	a, b   complex128
+	inside complex128
+	dir    Dir
+}
+
+func newSide(a, b, inside complex128) side {
+	if real(a) == real(b) {
+		if imag(a) < imag(b) {
+			return side{a, b, inside, V}
+		} else {
+			return side{b, a, inside, V}
+		}
+	} else {
+		if real(a) < real(b) {
+			return side{a, b, inside, H}
+		} else {
+			return side{b, a, inside, H}
+		}
+	}
+}
+
+func (s side) nextTo(o side) bool {
+	if s.dir != o.dir {
+		return false
+	}
+	if s.dir == H {
+		return (s.a == o.a+1i && s.b == o.b+1i && s.inside == o.inside+1i) || (s.a == o.a-1i && s.b == o.b-1i && s.inside == o.inside-1i)
+	} else {
+		return (s.a == o.a+1 && s.b == o.b+1 && s.inside == o.inside+1) || (s.a == o.a-1 && s.b == o.b-1 && s.inside == o.inside-1)
+	}
+}
+
+func part2(input *inputs.Grid) {
+	locs := map[complex128]rune{}
+	g := simple.NewUndirectedGraph()
+	nodes := map[complex128]graph.Node{}
+	locsFromNodeIds := map[int64]complex128{}
+	for loc, c := range input.Locs() {
+		locs[loc] = c
+		nodes[loc] = g.NewNode()
+		g.AddNode(nodes[loc])
+		locsFromNodeIds[nodes[loc].ID()] = loc
+	}
+	for loc, c := range locs {
+		if other, ok := locs[loc-1]; ok && c == other {
+			g.SetEdge(g.NewEdge(nodes[loc], nodes[loc-1]))
+		}
+		if other, ok := locs[loc+1]; ok && c == other {
+			g.SetEdge(g.NewEdge(nodes[loc], nodes[loc+1]))
+		}
+		if other, ok := locs[loc-1i]; ok && c == other {
+			g.SetEdge(g.NewEdge(nodes[loc], nodes[loc-1i]))
+		}
+		if other, ok := locs[loc+1i]; ok && c == other {
+			g.SetEdge(g.NewEdge(nodes[loc], nodes[loc+1i]))
+		}
+	}
+	s := 0
+	for _, component := range topo.ConnectedComponents(g) {
+		area := len(component)
+		compLocs := make(map[complex128]struct{}, len(component))
+		for _, n := range component {
+			compLocs[locsFromNodeIds[n.ID()]] = struct{}{}
+		}
+		sides := map[side]struct{}{}
+		for loc := range compLocs {
+			if _, ok := compLocs[loc-1]; !ok {
+				sides[newSide(loc, loc-1, loc)] = struct{}{}
+			}
+			if _, ok := compLocs[loc+1]; !ok {
+				sides[newSide(loc, loc+1, loc)] = struct{}{}
+			}
+			if _, ok := compLocs[loc-1i]; !ok {
+				sides[newSide(loc, loc-1i, loc)] = struct{}{}
+			}
+			if _, ok := compLocs[loc+1i]; !ok {
+				sides[newSide(loc, loc+1i, loc)] = struct{}{}
+			}
+		}
+		sg := simple.NewUndirectedGraph()
+		snodes := make(map[side]graph.Node, len(sides))
+		for s := range sides {
+			n := sg.NewNode()
+			sg.AddNode(n)
+			snodes[s] = n
+		}
+		for s := range sides {
+			for o := range sides {
+				if s != o && s.nextTo(o) {
+					sg.SetEdge(sg.NewEdge(snodes[s], snodes[o]))
+				}
+			}
+		}
+		numSides := len(topo.ConnectedComponents(sg))
+
+		s += area * numSides
+	}
+	fmt.Printf("Part 2: %d\n", s)
 }
