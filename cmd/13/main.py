821f1d4568869f571b3e0a2ddd479f4eab60dcd0
Ben Segall <bentekkie@google.com>
2024-12-13 15:00:57 +0000

    Format python

--- cmd/13/main.py
+++ cmd/13/main.py
@@ -1,14 +1,7 @@
 import os
 from typing import NamedTuple
-import numpy as np
-from scipy.optimize import milp, LinearConstraint, linprog, Bounds
+from scipy.optimize import linprog, Bounds
 from argparse import ArgumentParser
-
-'''
-Button A: X+94, Y+34
-Button B: X+22, Y+67
-Prize: X=8400, Y=5400
-'''
 
 THIS_FILE = os.path.abspath(__file__)
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -20,48 +13,68 @@ class Claw(NamedTuple):
     P: list[int]
 
 
-claws : list[Claw] = []
+claws: list[Claw] = []
 
 parser = ArgumentParser()
-parser.add_argument("--use_example", action='store_true')
+parser.add_argument("--use_example", action="store_true")
 args = parser.parse_args()
 
-fname = 'example.txt' if args.use_example else  'input.txt'
+fname = "example.txt" if args.use_example else "input.txt"
 
 day = os.path.basename(THIS_DIR)
 
-with open(os.path.join(THIS_DIR, '..','..','pkg','inputs','puzzle_inputs',day,fname)) as f:
+with open(
+    os.path.join(THIS_DIR, "..", "..", "pkg", "inputs", "puzzle_inputs", day, fname)
+) as f:
     A = []
     B = []
     P = []
     for line in f.readlines():
         if line.startswith("Button A: "):
-            A = [int(x) for x in line.removeprefix("Button A: ").replace("X+", "").replace("Y+", "").split(", ")]
+            A = [
+                int(x)
+                for x in line.removeprefix("Button A: ")
+                .replace("X+", "")
+                .replace("Y+", "")
+                .split(", ")
+            ]
         elif line.startswith("Button B: "):
-            B = [int(x) for x in line.removeprefix("Button B: ").replace("X+", "").replace("Y+", "").split(", ")]
+            B = [
+                int(x)
+                for x in line.removeprefix("Button B: ")
+                .replace("X+", "")
+                .replace("Y+", "")
+                .split(", ")
+            ]
         elif line.startswith("Prize: "):
-            P = [int(x) for x in line.removeprefix("Prize: ").replace("X=", "").replace("Y=", "").split(", ")]
+            P = [
+                int(x)
+                for x in line.removeprefix("Prize: ")
+                .replace("X=", "")
+                .replace("Y=", "")
+                .split(", ")
+            ]
         else:
-            claws.append(Claw(A,B,P))
+            claws.append(Claw(A, B, P))
             A = []
             B = []
             P = []
-    claws.append(Claw(A,B,P))
+    claws.append(Claw(A, B, P))
 
 
-def solve(A, B, P, bounds=None):
+def solve(A: list[int], B: list[int], P: list[int], bounds: tuple[int, int] | None = None):
     res = linprog(
-        method='highs',
-        c=[3,1],
-        A_eq=list(zip(A,B)),
+        c=[3, 1],
+        A_eq=list(zip(A, B)),
         b_eq=P,
-        bounds=bounds, 
+        bounds=bounds,
         integrality=[1, 1],
-        options={'presolve': False},
+        options={"presolve": False},
     )
     if res.x is None:
         return None
     return res.fun
+
 
 def part1():
     s = 0
@@ -71,6 +84,7 @@ def part1():
             s += c
     print(f"Part 1: {s}")
 
+
 def part2():
     s = 0
     for claw in claws:
@@ -78,5 +92,7 @@ def part2():
         if c is not None:
             s += c
     print(f"Part 2: {s}")
+
+
 part1()
 part2()
