bfa2e4e9e8a5a3095c186de254223978a6d5f611
Benjamin Segall <bentekkie@gmail.com>
2024-12-15 19:43:36 +0200

    Day 14 qand 15

--- cmd/14/main.go
+++ cmd/14/main.go
@@ -3,23 +3,165 @@ package main
 import (
 	"flag"
 	"fmt"
+	"iter"
+	"strconv"
+	"strings"
 
 	"github.com/bentekkie/advent_of_code_2024/pkg/inputs"
 )
 
 func main() {
 	flag.Parse()
-	input := inputs.String()
-	part1(input)
-	part2(input)
+	part1(inputs.Lines())
+	part2(inputs.Lines())
 }
 
-func part1(input string) {
-    // TODO: implement me
-	fmt.Printf("Part 1: \n")
+type Point struct {
+	x int64
+	y int64
 }
 
-func part2(input string) {
-    // TODO: implement me
-	fmt.Printf("Part 2: \n")
+type Velocity struct {
+	x int64
+	y int64
+}
+
+func posMod(n, m int64) int64 {
+	return (n%m + m) % m
+}
+
+func (p Point) quadrant(max Point) int {
+	if p.x < (max.x-1)/2 {
+		if p.y < (max.y-1)/2 {
+			return 1
+		} else if p.y > (max.y-1)/2 {
+			return 2
+		}
+	} else if p.x > (max.x-1)/2 {
+		if p.y < (max.y-1)/2 {
+			return 3
+		} else if p.y > (max.y-1)/2 {
+			return 4
+		}
+	}
+	return 0
+}
+
+func mustParseInt(s string) int64 {
+	n, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func part1(input iter.Seq[string]) {
+	robotMap := map[Point][]Velocity{}
+	for line := range input {
+		parts := strings.Split(strings.TrimSpace(line), " ")
+		if len(parts) != 2 {
+			continue
+		}
+		pparts := strings.Split(strings.TrimPrefix(parts[0], "p="), ",")
+		vparts := strings.Split(strings.TrimPrefix(parts[1], "v="), ",")
+		p := Point{mustParseInt(pparts[0]), mustParseInt(pparts[1])}
+		v := Velocity{mustParseInt(vparts[0]), mustParseInt(vparts[1])}
+		robotMap[p] = append(robotMap[p], v)
+	}
+	max := Point{101, 103}
+	for range 100 {
+		robotMap = moveRobots(robotMap, max)
+	}
+	quadCounts := map[int]int{}
+	for p, vs := range robotMap {
+		quadCounts[p.quadrant(max)] += len(vs)
+	}
+	fmt.Printf("Part 1: %v\n", quadCounts[1]*quadCounts[2]*quadCounts[3]*quadCounts[4])
+}
+
+func triangle(top Point, height int) iter.Seq[Point] {
+	return func(yield func(Point) bool) {
+		yield(top)
+		for row := range height - 1 {
+			for x := top.x - int64(row); x <= top.x+int64(row); x++ {
+				if !yield(Point{x, top.y + int64(row)}) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func containsTriangle(locs map[Point][]Velocity, top Point, height int) bool {
+	for p := range triangle(top, height) {
+		if vs, ok := locs[p]; !ok || len(vs) == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func largestTriangle(robots map[Point][]Velocity) int {
+	l := 0
+	for top := range robots {
+		height := l
+		for containsTriangle(robots, top, height) {
+			height++
+		}
+		l = height
+	}
+	return l
+}
+
+func grid(robots map[Point][]Velocity, max Point) string {
+	var sb strings.Builder
+	for y := range max.y {
+		for x := range max.x {
+			if len(robots[Point{x, y}]) > 0 {
+				sb.WriteRune('x')
+			} else {
+				sb.WriteRune('.')
+			}
+		}
+		sb.WriteRune('\n')
+	}
+	return sb.String()
+}
+
+func moveRobots(robotMap map[Point][]Velocity, max Point) map[Point][]Velocity {
+	nextRobotMap := make(map[Point][]Velocity, int(max.x*max.y))
+	for p, vs := range robotMap {
+		for _, v := range vs {
+			nextP := Point{
+				x: posMod(p.x+v.x, max.x),
+				y: posMod(p.y+v.y, max.y),
+			}
+			nextRobotMap[nextP] = append(nextRobotMap[nextP], v)
+		}
+	}
+	return nextRobotMap
+}
+
+func part2(input iter.Seq[string]) {
+	robotMap := map[Point][]Velocity{}
+	for line := range input {
+		parts := strings.Split(strings.TrimSpace(line), " ")
+		if len(parts) != 2 {
+			continue
+		}
+		pparts := strings.Split(strings.TrimPrefix(parts[0], "p="), ",")
+		vparts := strings.Split(strings.TrimPrefix(parts[1], "v="), ",")
+		p := Point{mustParseInt(pparts[0]), mustParseInt(pparts[1])}
+		v := Velocity{mustParseInt(vparts[0]), mustParseInt(vparts[1])}
+		robotMap[p] = append(robotMap[p], v)
+	}
+	max := Point{101, 103}
+	for k := range max.x * max.y {
+		robotMap = moveRobots(robotMap, max)
+		m := largestTriangle(robotMap)
+		if m > 5 {
+			fmt.Printf("%s\nPart 2: %d\n", grid(robotMap, max), k+1)
+			return
+		}
+	}
 }
--- cmd/15/main.go
+++ cmd/15/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"maps"
+	"slices"
+	"strings"
 
 	"github.com/bentekkie/advent_of_code_2024/pkg/inputs"
 )
@@ -14,12 +17,253 @@ func main() {
 	part2(input)
 }
 
+type Point struct {
+	x, y int
+}
+
+func (p Point) add(o Point) Point {
+	return Point{p.x + o.x, p.y + o.y}
+}
+
+type Box struct {
+	ps []Point
+}
+
+type Grid struct {
+	walls map[Point]struct{}
+	boxes map[Point]struct{}
+	robot Point
+}
+
+type WideGrid struct {
+	walls map[Point]struct{}
+	boxes map[Point]*Box
+	robot Point
+}
+
+func (g *Grid) isBox(p Point) bool {
+	_, ok := g.boxes[p]
+	return ok
+}
+
+func (g *Grid) isWall(p Point) bool {
+	_, ok := g.walls[p]
+	return ok
+}
+
+func parse(input string) (*Grid, string) {
+	parts := strings.Split(input, "\n\n")
+	g := &Grid{
+		walls: make(map[Point]struct{}),
+		boxes: make(map[Point]struct{}),
+	}
+	for y, line := range strings.Split(strings.TrimSpace(parts[0]), "\n") {
+		line = strings.TrimSpace(line)
+		for x, c := range line {
+			switch c {
+			case '#':
+				g.walls[Point{x, y}] = struct{}{}
+			case '@':
+				g.robot = Point{x, y}
+			case 'O':
+				g.boxes[Point{x, y}] = struct{}{}
+			}
+		}
+	}
+
+	return g, strings.ReplaceAll(parts[1], "\n", "")
+}
+
+func parseExpand(input string) (*WideGrid, string) {
+	parts := strings.Split(input, "\n\n")
+	g := &WideGrid{
+		walls: make(map[Point]struct{}),
+		boxes: make(map[Point]*Box),
+	}
+	for y, line := range strings.Split(strings.TrimSpace(parts[0]), "\n") {
+		line = strings.TrimSpace(line)
+		for x, c := range line {
+			switch c {
+			case '#':
+				g.walls[Point{2 * x, y}] = struct{}{}
+				g.walls[Point{2*x + 1, y}] = struct{}{}
+			case '@':
+				g.robot = Point{2 * x, y}
+			case 'O':
+				b := &Box{ps: []Point{{2 * x, y}, {2*x + 1, y}}}
+				g.boxes[Point{2 * x, y}] = b
+				g.boxes[Point{2*x + 1, y}] = b
+			}
+		}
+	}
+
+	return g, strings.ReplaceAll(parts[1], "\n", "")
+}
+
+func (g *WideGrid) boxAt(p Point) *Box {
+	return g.boxes[p]
+}
+
+func (g *WideGrid) isWall(p Point) bool {
+	_, ok := g.walls[p]
+	return ok
+}
+
 func part1(input string) {
-    // TODO: implement me
-	fmt.Printf("Part 1: \n")
+	g, inst := parse(input)
+	for _, c := range inst {
+		switch c {
+		case '<':
+			next := Point{g.robot.x - 1, g.robot.y}
+			if g.isWall(next) {
+				continue
+			}
+			if g.isBox(next) {
+				empty := next
+				for g.isBox(empty) {
+					empty.x--
+				}
+				if g.isWall(empty) {
+					continue
+				}
+				delete(g.boxes, next)
+				g.boxes[empty] = struct{}{}
+				g.robot.x--
+			} else {
+				g.robot.x--
+			}
+		case '>':
+			next := Point{g.robot.x + 1, g.robot.y}
+			if g.isWall(next) {
+				continue
+			}
+			if g.isBox(next) {
+				empty := next
+				for g.isBox(empty) {
+					empty.x++
+				}
+				if g.isWall(empty) {
+					continue
+				}
+				delete(g.boxes, next)
+				g.boxes[empty] = struct{}{}
+				g.robot.x++
+			} else {
+				g.robot.x++
+			}
+		case '^':
+			next := Point{g.robot.x, g.robot.y - 1}
+			if g.isWall(next) {
+				continue
+			}
+			if g.isBox(next) {
+				empty := next
+				for g.isBox(empty) {
+					empty.y--
+				}
+				if g.isWall(empty) {
+					continue
+				}
+				delete(g.boxes, next)
+				g.boxes[empty] = struct{}{}
+				g.robot.y--
+			} else {
+				g.robot.y--
+			}
+		case 'v':
+			next := Point{g.robot.x, g.robot.y + 1}
+			if g.isWall(next) {
+				continue
+			}
+			if g.isBox(next) {
+				empty := next
+				for g.isBox(empty) {
+					empty.y++
+				}
+				if g.isWall(empty) {
+					continue
+				}
+				delete(g.boxes, next)
+				g.boxes[empty] = struct{}{}
+				g.robot.y++
+			} else {
+				g.robot.y++
+			}
+		}
+	}
+	s := 0
+	for b := range g.boxes {
+		s += b.x + 100*b.y
+	}
+	fmt.Printf("Part 1: %d\n", s)
+}
+
+func (g *WideGrid) moveRobot(delta Point) {
+	next := g.robot.add(delta)
+	if g.isWall(next) {
+		return
+	}
+	nextBox := g.boxAt(next)
+	if nextBox == nil {
+		g.robot = next
+		return
+	}
+	boxes := map[*Box]struct{}{g.boxAt(next): {}}
+	toCheck := g.boxAt(next).ps
+	checked := map[Point]struct{}{}
+	for len(toCheck) > 0 {
+		newToCheck := map[Point]struct{}{}
+		for _, p := range toCheck {
+			if _, ok := checked[p]; ok {
+				continue
+			}
+			checked[p] = struct{}{}
+			pnext := p.add(delta)
+			if g.isWall(pnext) {
+				return
+			}
+			if b := g.boxAt(pnext); b != nil {
+				boxes[b] = struct{}{}
+				for _, p := range b.ps {
+					newToCheck[p] = struct{}{}
+				}
+			}
+		}
+		toCheck = slices.Collect(maps.Keys(newToCheck))
+	}
+	for b := range boxes {
+		for i := range b.ps {
+			delete(g.boxes, b.ps[i])
+			b.ps[i] = b.ps[i].add(delta)
+		}
+	}
+	for b := range boxes {
+		for i := range b.ps {
+			g.boxes[b.ps[i]] = b
+		}
+	}
+	g.robot = next
+}
+
+var deltas = map[rune]Point{
+	'<': {-1, 0},
+	'>': {1, 0},
+	'^': {0, -1},
+	'v': {0, 1},
 }
 
 func part2(input string) {
-    // TODO: implement me
-	fmt.Printf("Part 2: \n")
+	g, inst := parseExpand(input)
+	for _, c := range inst {
+		g.moveRobot(deltas[c])
+	}
+	s := 0
+	boxes := map[Point]struct{}{}
+	for _, b := range g.boxes {
+		boxes[b.ps[0]] = struct{}{}
+	}
+	for b := range boxes {
+		s += b.x + 100*b.y
+	}
+	fmt.Printf("Part 2: %d\n", s)
 }
